54c20b158b30d1f8a243e7e458afe2144d148c92
hbouabda <bouabda.hamza@gmail.com>
2020-04-25 18:23:26 +0100

    modify main application

--- src/main/java/com/skills/userService/UserServiceApplication.java
+++ src/main/java/com/skills/userService/UserServiceApplication.java
@@ -10,9 +10,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
-
 @SpringBootApplication
 //@EnableDiscoveryClient
 @ComponentScan({"com.skills"})
